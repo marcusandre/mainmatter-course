bc7cb9d1ac01b9f86f099c1af6e69c5e143a7a96
Marcus André <hello+github@marcusandre.de>
2024-06-23 18:09:21 +0200

    03: Ticket

--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -11,3 +11,59 @@
 // Integration here has a very specific meaning: they test **the public API** of your project.
 // You'll need to pay attention to the visibility of your types and methods; integration
 // tests can't access private or `pub(crate)` items.
+pub struct Order {
+    product_name: String,
+    quantity: i32,
+    unit_price: i32,
+}
+
+impl Order {
+    pub fn new(product_name: String, quantity: i32, unit_price: i32) -> Order {
+        validate_product_name(&product_name);
+        validate_amount(&quantity);
+        validate_amount(&unit_price);
+
+        Order {
+            product_name,
+            quantity,
+            unit_price,
+        }
+    }
+
+    pub fn product_name(&self) -> &str {
+        &self.product_name
+    }
+    pub fn set_product_name(&mut self, product_name: String) {
+        self.product_name = product_name;
+    }
+
+    pub fn quantity(&self) -> &i32 {
+        &self.quantity
+    }
+    pub fn set_quantity(&mut self, quantity: i32) {
+        self.quantity = quantity
+    }
+
+    pub fn unit_price(&self) -> &i32 {
+        &self.unit_price
+    }
+    pub fn set_unit_price(&mut self, unit_price: i32) {
+        self.unit_price = unit_price
+    }
+
+    pub fn total(&self) -> i32 {
+        &self.quantity * &self.unit_price
+    }
+}
+
+fn validate_product_name(product_name: &str) {
+    if product_name.is_empty() || product_name.as_bytes().len() > 300 {
+        panic!("Product name should not be empty or longer than 300 bytes.")
+    }
+}
+
+fn validate_amount(amount: &i32) {
+    if !amount.is_positive() {
+        panic!("Quantity must be a positive number")
+    }
+}
